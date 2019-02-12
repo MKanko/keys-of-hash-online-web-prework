c1e188a6a6ac0def3bc62834822a705082bc6bac
Mark Kanko <mkanko66@gmail.com>
2019-02-12 02:25:59 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,14 +2,11 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
- 
     species_key_array = []
     self.each do |species_key, habitat_value|
       arguments.each do |index|
-#binding.pry        
         if habitat_value == index
           species_key_array << species_key 
-
         end 
       end 
     end
@@ -17,9 +14,25 @@ class Hash
   end
 end
 
-# ""
+# "Putting the star before an object invokes the splat operator, 
+#  which has a variety of effects". So the splat operator does
+#  many different things, but in this case it is allowing for an 
+#  unknown amount of multiple arguments to be passed into the 
+#  above keys_of method, and the splat operator puts these 
+#  multiple arguments into an array.
 
-# arguments.each do |habitat_value|
+# So when there are multiple arguents being passed in as an array,
+# you will probably need to loop or iterate over each argument
+# which is an item in that array, like in the case above.
 
-# arguments passed in are in an array?
+# The hash that animals: => {} (key value pair) is a part of, is 
+# not named. So, to iterate over the animals: => {} (hash) you use 
+#  "self" to refer to the hash, because self is a context word referring 
+# to whatever you are calling #keys_of method on - which is a hash,
+# which is an "instance" of the class Hash. So whatever context
+# you are working in (writing code, defining a method, etc.) "self"
+# refers to the object or "instance" of that object within the 
+# context of that class.
+
+
 
