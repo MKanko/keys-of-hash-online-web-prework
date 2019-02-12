aecc17070aaf69d0e8cbe87b73d8de3c09920812
Mark Kanko <mkanko66@gmail.com>
2019-02-12 01:52:40 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,25 @@
 require 'pry'
 
 class Hash
-  def keys_of(arguments)
-    [:animals].each do |key, value|
-       *value 
-#binding.pry
+  def keys_of(*arguments)
+ 
+    species_key_array = []
+    self.each do |species_key, habitat_value|
+      arguments.each do |index|
+#binding.pry        
+        if habitat_value == index
+          species_key_array << species_key 
+
+        end 
+      end 
     end
-      
+    species_key_array  
   end
 end
+
+# In this case the splat operator is allowing for multiple arguments?
+
+# arguments.each do |habitat_value|
+
+# arguments passed in are in an array?
+
